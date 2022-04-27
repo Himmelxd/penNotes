20c73ad18e734c793b83cfdf37d3cbdef4642e13
Felix Beichler <felix@beichlers.de>
2022-04-27 09:24:37 +0200

    add fullscreen btn

--- main.js
+++ main.js
@@ -927,3 +927,25 @@ async function logMessage(title,text,color) {
     await new Promise(res => setTimeout(res, 1000));
     e.remove();
 }
+
+
+// Fullscreen Btn
+document.getElementsByTagName('html')[0].addEventListener('fullscreenchange', (event) => {
+    if (document.fullscreenElement) {
+        document.getElementById('toggle-fullscreen').innerHTML = `<svg style="width:24px;height:24px" viewBox="0 0 24 24">
+    <path fill="currentColor" d="M14,14H19V16H16V19H14V14M5,14H10V19H8V16H5V14M8,5H10V10H5V8H8V5M19,8V10H14V5H16V8H19Z" />
+  </svg>`;
+    } else {
+        document.getElementById('toggle-fullscreen').innerHTML = `<svg style="width:24px;height:24px" viewBox="0 0 24 24">
+    <path fill="currentColor" d="M5,5H10V7H7V10H5V5M14,5H19V10H17V7H14V5M17,14H19V19H14V17H17V14M10,17V19H5V14H7V17H10Z" />
+  </svg>`;
+    }
+});
+
+document.getElementById('toggle-fullscreen').addEventListener('click', (event) => {
+    if (document.fullscreenElement) {
+        document.exitFullscreen();
+    } else {
+        document.getElementsByTagName('html')[0].requestFullscreen();
+    }
+});
