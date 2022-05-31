2148fc475979ad8cc2cd6adbe69a5c6db5efec3d
Felix Beichler <felix.git@beichlers.de>
2022-06-01 01:29:45 +0200

    zwitch + and eraser

--- main.js
+++ main.js
@@ -726,6 +726,13 @@ function makePenList(list) {
         penList.appendChild(e);
     }
 
+    
+    e = document.createElement('div');
+    e.innerHTML = `<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="currentColor" d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" /></svg>`;
+    e.style.color = 'darkgreen';
+    e.onclick = addPen;
+    penList.appendChild(e);
+    
     e = document.createElement('div');
     e.classList = 'eraser';
     e.id = 'eraser';
@@ -733,12 +740,6 @@ function makePenList(list) {
     e.style.color = 'salmon';
     e.setAttribute("type","eraser")
     e.setAttribute("onclick", "currentPen = this;this.classList.add('active');penList.querySelector('.pen.active').classList.remove('active');");
-    penList.appendChild(e);
-
-    e = document.createElement('div');
-    e.innerHTML = `<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="currentColor" d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" /></svg>`;
-    e.style.color = 'darkgreen';
-    e.onclick = addPen;
     penList.appendChild(e);
     
 }
